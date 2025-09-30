f431361522ad927fc60e3e0dd1e478109de9e2a4
WCY-Hugo <chunyiuhugo.wong@gmail.com>
2025-09-30 10:47:14 +0800

    Create 2221-find-triangular-sum-of-an-array.ts

---
+++ 2221-Find-Triangular-Sum-of-an-Array/2221-find-triangular-sum-of-an-array.ts
@@ -0,0 +1,10 @@
+function triangularSum(nums: number[]): number {
+  while (nums.length > 1) {
+    for (let i = 0; i < nums.length - 1; i++) {
+      nums[i] = (nums[i] + nums[i + 1]) % 10;
+    }
+    nums.pop();
+  }
+
+  return nums[0];
+}
